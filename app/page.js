bb85c89ea128f091c36082cc58156c66573630ae
yannjrdn <yannjourdan@outlook.fr>
2025-01-29 16:41:14 +0100

    fix pour l'erreur

--- app/page.js
+++ app/page.js
@@ -8,6 +8,18 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
+import dynamic from 'next/dynamic';
+import { personalData } from "@/utils/data/personal-data";
+
+// Dynamically import all components with SSR: false to avoid rendering them during SSR
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
