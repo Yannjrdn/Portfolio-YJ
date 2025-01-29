9a4a6128c0f59d43ec569d6688ce1fc33026ec80
yannjrdn <yannjourdan@outlook.fr>
2025-01-29 16:45:04 +0100

    fix 2

--- app/page.js
+++ app/page.js
@@ -1,13 +1,3 @@
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
-
 import dynamic from 'next/dynamic';
 import { personalData } from "@/utils/data/personal-data";
 
@@ -19,7 +9,6 @@ const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: fals
 const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
 const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
-
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
